b5062c2680cc3678bf2be7d5e0d7d0573f525c4d
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-02-24 23:47:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
-#include "LCD/LCD_Config.h"
 #include "Keypad/Keypad_Comfig.h"
 
-int main(void){	
-	LCD_Init(&PIND, &PIND, 0, 1, 2, 3, 4, 5, 6);
+int main( void ){
 	Keypad_Init();
-	
+	DDRA = 0xFF;
 	u8 Keypad_Read = 0;
-	u8 Element = '0';
-	
-    while (1){
+	while (1){
 		Keypad_Read = Keypad_Scan();
 		if(Keypad_Read != 0)
-			LCD_Char(Keypad_Read);
-    }
+			PORTA = Keypad_Read;
+	}
 }
